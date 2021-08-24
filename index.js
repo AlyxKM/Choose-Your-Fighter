7ddd524185d6c4e0d9b4ba38ea0986a2c1d0182e
alyxandria meadows <alyxmeadows@yahoo.com>
2021-08-23 19:05:54 -0500

    updated index.html w first dropdown

---
+++ index.js
@@ -0,0 +1,10 @@
+// const Url = "https://gateway.marvel.com/v1/public/characters/1011334?apikey=a0e785c96f13e6b9db090ca904788838"
+
+// fetch(Url)
+// .then(res => res.json)
+// .then(data => console.log(data.name))
+
+// console.log("hello!")
+
+// a0e785c96f13e6b9db090ca904788838 marvel api public key
+
