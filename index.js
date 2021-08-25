d6260f14ba55c6455c022c65dd4a324ba646cfc5
alyxandria meadows <alyxmeadows@yahoo.com>
2021-08-24 19:34:11 -0500

    fixed api added characters as variables

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
-const Url = "https://superheroapi.com/api/10218974527823084/2"
-fetch(Url, {
-    method: "GET",
-    headers: {
-        "Content-Type": "application/json"
-    },
-    mode: "no-cors"
-})
-
+const Url = "https://www.amiiboapi.com/api/amiibo"
+fetch(Url)
 .then(res => res.json())
-.then(data => console.log(data))
+.then(data => {
+    const marioObj = data.amiibo[0];
+    const peachObj = data.amiibo[14];
+    const linkObj = data.amiibo[51];
+    const zeldaObj = data.amiibo[59];
+    const samusObj = data.amiibo[555];
+    const pikachuObj = data.amiibo[684];
+    const kirbyObj = data.amiibo[695];
+    const nessObj = data.amiibo[716];
 
-console.log("hello!")
-
-//10218974527823084
+    console.log(nessObj.name);
+    console.log(nessObj.gameSeries);
+    
+//mario[0],link[51],zelda[59],peach[14],kirby[695],pikachu[684/692],samus[555],ness[716]
+// keys include name, image, gameSeries
+})
