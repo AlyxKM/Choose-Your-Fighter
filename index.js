7b02e04c0cb00ed97e8f6bc34c6eb69609c6fcfc
alyxandria meadows <alyxmeadows@yahoo.com>
2021-08-25 17:15:01 -0500

    added img and names to fighter values

--- index.js
+++ index.js
@@ -2,18 +2,48 @@ const Url = "https://www.amiiboapi.com/api/amiibo"
 fetch(Url)
 .then(res => res.json())
 .then(data => {
-    const marioObj = data.amiibo[0];
-    const peachObj = data.amiibo[14];
-    const linkObj = data.amiibo[51];
-    const zeldaObj = data.amiibo[59];
-    const samusObj = data.amiibo[555];
-    const pikachuObj = data.amiibo[684];
-    const kirbyObj = data.amiibo[695];
-    const nessObj = data.amiibo[716];
+    const mario = data.amiibo[0];
+    const peach = data.amiibo[14];
+    const link = data.amiibo[51];
+    const zelda = data.amiibo[59];
+    const samus = data.amiibo[555];
+    const pikachu = data.amiibo[684];
+    const kirby = data.amiibo[695];
+    const ness = data.amiibo[716];
 
-    console.log(nessObj.name);
-    console.log(nessObj.gameSeries);
+    const fightArr = [mario, peach, kirby, ness];
+    const oppArr = [link, zelda, samus, pikachu];
+
+    const selectFighter = document.querySelector(".select-fighter");
+    const selectOpponent = document.querySelector(".select-opponent");
+
+    // console.log(fightArr);
+    // console.log(oppArr);
+
+    const pickCharacter = charObj => {  
+        const charName = document.getElementById("character-name");
+        const charImg = document.getElementById("character-image");
+        charName.textContent = charObj.name;
+        charImg.src = charObj.image
+    };
+
+    fightArr.forEach(charObj => {
+        selectFighter.addEventListener("change", event => {
+            event.preventDefault();
+            pickCharacter(charObj);
+            console.log(event.target.value)
+         });
+    });
+    
+
+    
+
+});
+
+
+
+    // console.log(nessObj.name);
+    // console.log(nessObj.image);
     
 //mario[0],link[51],zelda[59],peach[14],kirby[695],pikachu[684/692],samus[555],ness[716]
 // keys include name, image, gameSeries
-})
